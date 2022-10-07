c1b6fa85ec9c18f92aab66e8fc11501a876b8fdc
Kevin Huang <kev.y.huang@gmail.com>
2022-10-07 11:49:06 -0400

    Adding comments

--- app.py
+++ app.py
@@ -3,23 +3,27 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# /// = relative path, //// = absolute path
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# SQLAlchemy config
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-
+# SQLAlchemy Todo schema
 class Todo(db.Model):
+    # id, title, and complete columns
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
 
+# Home function to fetch todo_list from db
 @app.route("/")
 def home():
     todo_list = Todo.query.all()
     return render_template("base.html", todo_list=todo_list)
 
+
+# POST end point for creating new todos
 @app.route("/add", methods=["POST"])
 def add():
     title = request.form.get("title")
@@ -28,6 +32,8 @@ def add():
     db.session.commit()
     return redirect(url_for("home"))
 
+
+# Update function to change todo completion status
 @app.route("/update/<int:todo_id>")
 def update(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
@@ -35,12 +41,15 @@ def update(todo_id):
     db.session.commit()
     return redirect(url_for("home"))
 
+
+# Delete function to delete the todo from the db
 @app.route("/delete/<int:todo_id>")
 def delete(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("home"))
+
 
 if __name__ == "__main__":
     db.create_all()
