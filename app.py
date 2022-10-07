9082b244449441de587abab4dd84efec5b9edb64
Kevin Huang <kev.y.huang@gmail.com>
2022-10-07 12:05:05 -0400

    Setting up project for hosting on heroku with sql db
    new proc file
    
    update to procfile
    
    proc
    
    proc
    
    Using heroku postgres instead of sqlite
    
    correct module postgres --> postgresql
    
    add support for psycopg2
    
    adding app_context
    
    more setup for todo modeo
    
    correct comment

--- app.py
+++ app.py
@@ -4,16 +4,25 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # SQLAlchemy config
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql://opvtxqswlcgjvs:cb425c748e0dc47a02c4569133cd8808c862b14019ba9e763b190a9f0f253e5b@ec2-3-93-206-109.compute-1.amazonaws.com:5432/d6gtmdbdi5bmhf"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 # SQLAlchemy Todo schema
 class Todo(db.Model):
+    __tablename__ = "todo"
+
     # id, title, and complete columns
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
+
+    # Constructor function
+    def __init__(self, title, complete):
+        self.title = title
+        self.complete = complete
 
 
 # Home function to fetch todo_list from db
@@ -27,7 +36,7 @@ def home():
 @app.route("/add", methods=["POST"])
 def add():
     title = request.form.get("title")
-    new_todo = Todo(title=title, complete=False)
+    new_todo = Todo(title, False)
     db.session.add(new_todo)
     db.session.commit()
     return redirect(url_for("home"))
